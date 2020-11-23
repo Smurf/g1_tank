2c44d1b1111eb6068dd07dced8a646c6e3ecd5d8
Sam Coles <sammuel.coles@gmail.com>
2020-11-23 00:09:25 -0600

    Update readme, create scripts for single cameras

---
+++ 02-twitch-control/start_stream.sh
@@ -0,0 +1,24 @@
+INRES="1280x480" # input resolution
+OUTRES="1280x480" # output resolution
+FPS="30" # target FPS
+GOP="60" # i-frame interval, should be double of FPS, 
+GOPMIN="30" # min i-frame interval, should be equal to fps, 
+THREADS="6" # max 6
+CBR="1000k" # constant bitrate (should be between 1000k - 3000k)
+QUALITY="ultrafast"  # one of the many FFMPEG preset
+AUDIO_RATE="44100"
+STREAM_KEY="$1" # use the terminal command Streaming streamkeyhere to stream your video to twitch or justin
+SERVER="live-ord03" # twitch server in frankfurt, see https://stream.twitch.tv/ingests/ for list
+
+ffmpeg -re -an -i /dev/video0 -f video4linux2 -framerate 30 -s 640x480 \
+-filter_complex " \
+[0:v] drawtext='
+fontfile=media/proggy-clean-ttszbp-1361506269.ttf:
+fontsize=40:
+fontcolor=white:
+x=0:y=0:
+textfile=tank_telemetry.txt:reload=1'" \
+-f flv \
+-vcodec h264_omx -profile:v baseline -level:v 4.0 -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p \
+-s $OUTRES -threads $THREADS -strict normal \
+-bufsize $CBR "rtmp://$SERVER.twitch.tv/app/$1"
---
+++ README.md
@@ -0,0 +1,24 @@
+# Yahboom G1 Tank Examples
+
+This repository contains example code for controlling the [Yahboom G1 Tank Raspberry Pi kit](https://www.yahboom.net/study/G1-T-PI).
+
+# Overview
+
+READMEs are currently being generated for each example.
+
+### 01-local-ssh-control
+
+An example of using streameye to create a local stream on your LAN. The tank is controlled via keypresses in a SSH session.
+
+### 02-twitch-control
+
+This is a more complex example where the tank camera output is sent to a twitch.tv stream and can be controlled by chat.
+
+### 03-web-control
+
+Run a web server on the tank and host a low latency (~1 second) stream.
+
+# TODO
+
+- [ ] Add READMEs for each example with usage examples
+
